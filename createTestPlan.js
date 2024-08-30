e77fc257226494796b659c887feeba9fbafb5406
hitesh <hitesh@gigapromoters.com>
2024-08-30 12:36:47 +0530

    changes suggested by Vishal

--- createTestPlan.js
+++ createTestPlan.js
@@ -6,11 +6,12 @@ require('dotenv').config();
 const fetch = require('node-fetch');
 const { getDate, getTCConfig } = require('./utils');
 //testcollab-sdk
-const { TestPlansApi, TestPlanTestCasesApi, TestPlansAssignmentApi } = require('testcollab-sdk');
+const { TestPlansApi, TestPlanTestCasesApi, TestPlansAssignmentApi, TestCasesApi } = require('testcollab-sdk');
 
 let config = getTCConfig()
 
 // init apis
+const testCaseApi = new TestCasesApi(config);
 const testPlansApi = new TestPlansApi(config);
 const testPlanCases = new TestPlanTestCasesApi(config);
 const testPlanAssignment = new TestPlansAssignmentApi(config);
@@ -21,19 +22,32 @@ const createTestPlan = async () => {
     require('fs').unlink('tmp/tc_test_plan', () => { });
 
     try {
+        let testCaseCount = 50;
         //const response = await testPlansApi.addTestPlan(testPlan);
-        console.log('Step 1: Creating a new test plan...')
+        try {
+            //this is needed for testPlanTestCaseBulkAddPayload
+            const respTCCount = await testCaseApi.getTestCaseCount({
+                "project": process.env.TESTCOLLAB_PROJECT_ID
+            });
+            if (respTCCount && respTCCount.count) {
+                testCaseCount = respTCCount.count;
+            }
+        } catch (e) {
+            console.log("project test case count error", e);
+        }
+
+        console.log('Step 1: Creating a new test plan...');
 
         const response = await testPlansApi.addTestPlan({
             testPlanPayload: {
                 project: process.env.TESTCOLLAB_PROJECT_ID,
-                title: 'CI Test: '+getDate(),
+                title: 'CI Test: ' + getDate(),
                 description: 'This is a test plan created using the Node.js SDK',
                 status: 1,
                 priority: 1,
                 testPlanFolder: null,
+                //@TODO: handle cases when there is a mandatory custom field in test plan
                 customFields: []
-
             }
         });
 
@@ -54,7 +68,7 @@ const createTestPlan = async () => {
                     selector: [
                         {
                             "field": "tags",
-                            "operator": "jsonstring_2",
+                            "operator": "jsonstring_" + testCaseCount,
                             "value": "{\"filter\":[[" + process.env.TESTCOLLAB_CI_TAG_ID + "]],\"type\":\"equals\",\"filterType\":\"number\"}"
                         }
                     ]
@@ -74,7 +88,7 @@ const createTestPlan = async () => {
                 "assignmentCriteria": "testCase",
                 "assignmentMethod": "automatic",
                 "assignment": {
-                    "user": [2],
+                    "user": [process.env.TESTCOLLAB_ASSIGNEE_ID],
                     "testCases": {
                         "testCases": [],
                         "selector": []
@@ -86,11 +100,11 @@ const createTestPlan = async () => {
             }
         })
 
-        console.log(res3)
+        // console.log(res3)
 
         // store test plan id in tmp/test_plan
         //console.log('Test Plan ID: ' + testPlanId)
-        require('fs').writeFileSync('tmp/tc_test_plan', 'TESTCOLLAB_TEST_PLAN_ID='+testPlanId);
+        require('fs').writeFileSync('tmp/tc_test_plan', 'TESTCOLLAB_TEST_PLAN_ID=' + testPlanId);
 
     } catch (error) {
         console.error(error);
